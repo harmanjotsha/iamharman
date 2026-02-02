4d70c25b4cc7225ca734c13ed2f02aec8e43362d
harmanjot2345 <harmanjotsharma914@gmail.com>
2026-02-02 16:41:54 +0530

    Fix CSS loading issue after deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/Dashboard.css'
-import './styles/Landing.css'
-import './styles/Auth.css'
-import './styles/Search.css'
-import './styles/Appointments.css'
-import './styles/Profile.css'
-import './styles/Records.css'
-import './styles/Layout.css'
-import './styles/Booking.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
